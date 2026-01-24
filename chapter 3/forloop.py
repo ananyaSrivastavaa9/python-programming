62cb1e2c35935ad3149a2c7fdd1d60a77ded3186
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-01-25 02:28:39 +0530

    Chapter 3: added for loop programs

---
+++ chapter 3/forloop.py
@@ -0,0 +1,25 @@
+# for loop basic
+name = "Ananya"
+for i in name:
+    print(name)
+    if (i == "n"):
+        print("wow")
+
+# for loop over list
+colors = ["Red", "Green", "Blue", "Orange"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+# for loop over range
+for i in range(7):
+    print(i)
+
+# between two numbers
+for i in range(1,50):
+    print(i)
+
+# three value
+for i in range(1,50,4):
+    print(i)
