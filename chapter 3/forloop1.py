66d44f02f363b78263079570052f3f869f9f1bcb
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-01-25 19:14:31 +0530

    Chapter 3: added while loop examples

---
+++ chapter 3/forloop1.py
@@ -0,0 +1,8 @@
+for i in range(1,11):
+    if (i % 2 == 0):
+        print(i)
+
+# table
+n = 2
+for i in range(1,11):
+    print(n, "*", i, "=", n*i)
---
+++ chapter 3/whileloop.py
@@ -0,0 +1,20 @@
+# example 1
+i = int(input("enter the value of i : "))
+while (i <= 50):
+    print(i)
+    i = i + 1 #increment
+
+# example 2
+count = 5
+while (count > 0):
+    print(count)
+    count = count - 1 #decrement
+
+# else with while loop
+count = 9
+while (count > 0):
+    print(count)
+    count = count - 1
+else :
+    print("Condition not satisfy")
+
