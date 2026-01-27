10ab0fa1eae2e4db313f371c2a286704bc8b74d8
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-01-28 03:00:18 +0530

    break and continue statement added

---
+++ chapter 3/break.py
@@ -0,0 +1,5 @@
+n = 12
+for i in range(1,11):
+    if i == 4:
+        break
+    print("12*",i,"=",n*i)
---
+++ chapter 3/continue.py
@@ -0,0 +1,5 @@
+n = 10
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(n,"*",i,"=",n*i)
