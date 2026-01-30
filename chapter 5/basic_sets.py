79ae51d92b0a44b4187bbd415b58eb8871b6247a
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-01-31 03:18:55 +0530

    added chapter 5: sets anddictionaries

---
+++ chapter 5/basic_sets.py
@@ -0,0 +1,16 @@
+# do not take repeated values
+s = {2,4,2,6,7}
+print(s)
+
+# different data types
+a = {2,"ananya",False,2.8}
+print(a)
+
+# empty set
+b = set()
+print(type(b))
+
+# accessing set items
+# 1) using FOR loop
+for items in a:
+    print(items)
