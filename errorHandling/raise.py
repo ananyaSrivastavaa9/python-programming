15014aa5e31f90f1763e7dd3d61821b26e628837
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-08 01:57:06 +0530

    changes made

--- errorHandling/raise.py
+++ errorHandling/raise.py
@@ -1,9 +1,9 @@
 # raising custom errors 
 
-# age = -5
-# if age < 0:
-#     raise ValueError("Age negative nahi ho sakti")
+age = -5
+if age < 0:
+    raise ValueError("Age negative nahi ho sakti")
 
-marks = 120
-if marks > 100:
-    raise Exception("Marks 100 se zyada nahi ho sakte")
+# marks = 120
+# if marks > 100:
+#     raise Exception("Marks 100 se zyada nahi ho sakte")
