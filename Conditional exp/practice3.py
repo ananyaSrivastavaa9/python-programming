9b8c1369ebb1346e450126a6dd67b00400005834
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-08 01:55:59 +0530

    changes made

--- Conditional exp/practice3.py
+++ Conditional exp/practice3.py
@@ -1,6 +1,6 @@
 # Write a program to detect double space in a string. 
-a = input("enter a string: ")
+a = input("enter a string:")
 if "  " in a:
-    print("double space present")
+    print("double space Present")
 else:
     print("no double space")
