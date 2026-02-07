49a0afca939a531ee07d20cc910eae21c20f2df4
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-08 01:58:54 +0530

    changes made

--- lists&tuples/T1.py
+++ lists&tuples/T1.py
@@ -1,11 +1,11 @@
 # Write a program to store seven fruits in a list entered by the user.
 
-# fruits = ["apple","banana","orange","mango","kiwi","pineapple","custardFruit"]
-# print(fruits)
+fruits = ["apple","banana","orange","mango","kiwi","pineapple","custardFruit"]
+print(fruits)
 
-fruits = []
-for i in range(7):
-    fruit = input("Enter fruit name: ")
-    fruits.append(fruit)
+# fruits = []
+# for i in range(7):
+#     fruit = input("Enter fruit name: ")
+#     fruits.append(fruit)
 
-print(fruits) 
+# print(fruits) 
