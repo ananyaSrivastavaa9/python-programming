8bde25e068bf1bd21012e06bd04c43c74b4dcff4
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-08 01:59:50 +0530

    changes made

--- lists&tuples/T2.py
+++ lists&tuples/T2.py
@@ -1,6 +1,6 @@
 # Store N items entered by user in a list
 
-# list_name = []
-# for i in range(N):
-#     value = input()
-#     list_name.append(value)
+list_name = []
+for i in range(N):
+    value = input()
+    list_name.append(value)
