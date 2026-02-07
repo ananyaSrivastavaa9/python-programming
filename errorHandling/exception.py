f1eee1902de17cad0a143e23ccbde550f2bef609
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-08 01:56:31 +0530

    changes made

--- errorHandling/exception.py
+++ errorHandling/exception.py
@@ -1,14 +1,14 @@
 # exception handling
 
-# a = input("enter a number: ")
-# print(f"multiplication table of {a} is: ")
-# try:
-#     for i in range(1,11):
-#         print(f"{int(a)} X {int(i)} = {int(a)*int(i)}")
-# except:
-#     print("sorry ")
-
+a = input("enter a number: ")
+print(f"multiplication table of {a} is: ")
 try:
-    num = int(input("enter a num: "))
+    for i in range(1,11):
+        print(f"{int(a)} X {int(i)} = {int(a)*int(i)}")
 except:
-    print("no entered is not interger")
+    print("sorry ")
+
+# try:
+#     num = int(input("enter a num: "))
+# except:
+#     print("no entered is not interger")
