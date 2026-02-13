cfe6e4a9d3d614a6c0188ad8d16ca73485b950e9
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-14 01:34:00 +0530

    Added functions

--- function&recursion/Farguments.py
+++ function&recursion/Farguments.py
@@ -14,7 +14,39 @@ def name(name, age, section):
 name(age = 18, section = "B", name = "Ananya") #order does not matter
 
 #required arguments
-def subtract(a,b):
-    print("difference is :",(a-b))
-subtract()
 
+# def subtract(a,b):
+#     print("difference is :",(a-b))
+# subtract()
+
+# variable-length arguments
+
+    # *args(arbitary arguments)
+def name(*name):
+    print("Hello,", name[0], name[1], name[2])
+name("Ms.", "Ananya", "Srivastava")
+
+# eg 2
+def add_num(*num):
+    total = 0
+    for number in num:
+        total += number
+    return total
+
+print(add_num(1,2,3))
+
+    # Keyword Arbitrary Arguments (**kwargs)
+def greet(**wish):
+    for key,value in wish.items():
+        print(key, "says", value)
+greet(Ananya = "Hi", Minal = "hey", Priya = "hello")
+
+# return statement
+def sum(a,b):
+    sum = a+b
+    return sum
+print(sum(1,2))
+
+def name(name1, name2, name3):
+    return "Hello" + " " + name1 + " " + name2 + " " + name3
+print(name("Ananya", "Minal", "Priya"))
