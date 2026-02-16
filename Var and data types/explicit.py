3f5db0c8f93ebdcab1468024c771c31babc5227a
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-16 10:04:19 +0530

    ques added

---
+++ Var and data types/explicit.py
@@ -0,0 +1,7 @@
+# Explicit (Manual) Type Conversion:
+# You manually convert one data type into another using type casting functions like int(), float(), str().
+
+a = 10
+b = 34
+c = float(a+b)
+print(c)
---
+++ Var and data types/implicit.py
@@ -0,0 +1,5 @@
+# Python automatically converts one data type to another without user instruction to avoid data loss.
+#implicit typeconversion
+a = 9.5
+b = 2
+print(a+b)
