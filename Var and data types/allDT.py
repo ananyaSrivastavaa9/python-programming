0f4bb48c6bb4b2c47eceb75d37b9a9e22999df1e
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-18 20:58:24 +0530

    ques added

---
+++ Var and data types/allDT.py
@@ -0,0 +1,27 @@
+# Integer
+age = 25
+
+# Float
+height = 5.9
+
+# String
+name = "Alice"
+
+# Boolean
+is_student = True
+
+# List
+fruits = ["apple","banana","cherry"]
+
+# Tuple
+coordinates = (10,20)
+
+# Dictionary
+person = {"name":"Alice", "age":25}
+
+# Set
+unique_numbers = {1,2,3,4,5}
+
+# Printing
+print(f"Name: {name}, Age: {age}, Height: {height}, Student: {is_student}")
+print(f"Fruits: {fruits}, Coordinates: {coordinates}, Person: {person}, Unique Numbers: {unique_numbers}")
