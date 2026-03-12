07215478d4a05704461b59a948f37ff12a7bbcca
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-03-13 01:43:25 +0530

    Added math function - alias, floor and ceil, power, pi, e, specific function

---
+++ function&recursion/mathfunction.py
@@ -0,0 +1,20 @@
+import math as m #alias
+x = m.sqrt(25)
+print(x)
+
+# floor and ceil function
+print(m.floor(2.9)) #2
+print(m.ceil(2.1)) #3
+
+# power function
+print(m.pow(5,2)) #25.0
+
+# pi
+print(m.pi) #3.141592653589793
+
+# e
+print(m.e) #2.718281828459045
+
+# want some specific function
+from math import pow, sqrt
+print(pow(3,2))
