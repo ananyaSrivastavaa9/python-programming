9d522ac660dbc20bd5fffe50c4ece6d342b1dccf
Ananya Srivastava <227468494+ananyaSrivastavaa9@users.noreply.github.com>
2026-03-16 23:35:54 +0530

    Fix slicing example and remove extra newline

--- strings/slicingpython.py
+++ strings/slicingpython.py
@@ -16,7 +16,6 @@ print(pie[ : 5]) #slicing from start
 print(pie[1: ]) #slicing till end
 print(pie[2:6]) #slicing in between
 print(pie[-3:-2]) #slicing in negative index
-
 # quick quiz
 nm = "Harry"
 print(nm[-4:-2])
