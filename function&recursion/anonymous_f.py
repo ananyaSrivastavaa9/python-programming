0a0294fa2921048eaac8b6f6d53a5003c05e0059
Ananya Srivastava <your_github_username@users.noreply.github.com>
2026-03-24 00:02:26 +0530

    decorators

--- function&recursion/anonymous_f.py
+++ function&recursion/anonymous_f.py
@@ -7,3 +7,10 @@ print(result)
 f = lambda a,b : a+b
 result = f(5,3)
 print(result)
+
+# filter()
+def is_even(a):
+    return a%2==0
+nums = [1,2,3,4,5,6,7,8,9,10]
+result = list(filter(is_even,nums))
+print(result)
