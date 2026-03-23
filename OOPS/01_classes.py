e482bd2950eb59d9f0a25070e7b2f6fae16cb53a
Ananya Srivastava <your_github_username@users.noreply.github.com>
2026-03-24 03:59:02 +0530

    classes

---
+++ OOPS/01_classes.py
@@ -0,0 +1,19 @@
+## classes
+# class Student:
+#     name = "Ananya"
+
+## objects(instance)
+# s1 = Student()
+# print(s1.name)
+
+# Car Classes and objects
+class Car:
+    color = "blue"
+    brand = "BMW"
+    design = "black"
+    owner = "Ananya"
+c1 = Car()
+print(c1.color)
+print(c1.brand)
+print(c1.design)
+print(c1.owner)
